0d717d030e64d38287cd3c1bb0854f1cceb62b4c
casid <andy@mazebert.com>
2016-07-04 22:11:16 +0200

    Increase test readability

--- src/test/java/org/jusecase/katas/gildedrose/UpdateItemsTest.java
+++ src/test/java/org/jusecase/katas/gildedrose/UpdateItemsTest.java
@@ -27,81 +27,77 @@ public class UpdateItemsTest extends UsecaseTest<Request, Response> {
 
     @Test
     public void qualityIsReduced() {
-        givenItems(an(item().standard().withQuality(10)));
+        givenItem(an(item().standard().withQuality(10)));
         whenItemsAreUpdated();
         thenItemQualityIs(9);
     }
 
-    private void whenItemsAreUpdated() {
-        whenRequestIsExecuted();
-    }
-
     @Test
     public void sellInIsReduced() {
-        givenItems(an(item().standard().withSellIn(5)));
+        givenItem(an(item().standard().withSellIn(5)));
         whenItemsAreUpdated();
         thenItemSellInIs(4);
     }
 
     @Test
     public void sellInPassed_qualityDegradesTwiceAsFast() {
-        givenItems(an(item().standard().withSellIn(-10).withQuality(10)));
+        givenItem(an(item().standard().withSellIn(-10).withQuality(10)));
         whenItemsAreUpdated();
         thenItemQualityIs(8);
     }
 
     @Test
     public void sellInReachesZero_qualityDegradesRegulary() {
-        givenItems(an(item().standard().withSellIn(1).withQuality(10)));
+        givenItem(an(item().standard().withSellIn(1).withQuality(10)));
         whenItemsAreUpdated();
         thenItemQualityIs(9);
     }
 
     @Test
     public void sellInBecomesNegative_qualityDegradesRegulary() {
-        givenItems(an(item().standard().withSellIn(0).withQuality(10)));
+        givenItem(an(item().standard().withSellIn(0).withQuality(10)));
         whenItemsAreUpdated();
         thenItemQualityIs(9);
     }
 
     @Test
     public void itemQualityCannotBecomeNegative() {
-        givenItems(an(item().standard().withQuality(0)));
+        givenItem(an(item().standard().withQuality(0)));
         whenItemsAreUpdated();
         thenItemQualityIs(0);
     }
 
     @Test
     public void itemQualityCannotBecomeNegative_evenWhenSellInDateIsReached() {
-        givenItems(an(item().standard().withQuality(1).withSellIn(-1)));
+        givenItem(an(item().standard().withQuality(1).withSellIn(-1)));
         whenItemsAreUpdated();
         thenItemQualityIs(0);
     }
 
     @Test
     public void agedBree_increasesInQuality() {
-        givenItems(an(item().agedBree().withQuality(1)));
+        givenItem(an(item().agedBree().withQuality(1)));
         whenItemsAreUpdated();
         thenItemQualityIs(2);
     }
 
     @Test
     public void agedBree_maxQuality() {
-        givenItems(an(item().agedBree().withQuality(50)));
+        givenItem(an(item().agedBree().withQuality(50)));
         whenItemsAreUpdated();
         thenItemQualityIs(50);
     }
 
     @Test
     public void agedBree_sellInIsReduced() {
-        givenItems(an(item().agedBree().withSellIn(10)));
+        givenItem(an(item().agedBree().withSellIn(10)));
         whenItemsAreUpdated();
         thenItemSellInIs(9);
     }
 
     @Test
     public void legendaryItemsAreTimeless() {
-        givenItems(an(item().sulfuras()));
+        givenItem(an(item().sulfuras()));
 
         whenItemsAreUpdated();
 
@@ -111,63 +107,63 @@ public class UpdateItemsTest extends UsecaseTest<Request, Response> {
 
     @Test
     public void backstagePass_sellInIsReduced() {
-        givenItems(an(item().backstagePass().withSellIn(5)));
+        givenItem(an(item().backstagePass().withSellIn(5)));
         whenItemsAreUpdated();
         thenItemSellInIs(4);
     }
 
     @Test
     public void backstagePass_normalIncreaseInQuality() {
-        givenItems(an(item().backstagePass().withSellIn(11).withQuality(10)));
+        givenItem(an(item().backstagePass().withSellIn(11).withQuality(10)));
         whenItemsAreUpdated();
         thenItemQualityIs(11);
     }
 
     @Test
     public void backstagePass_doubleIncreaseInQuality() {
-        givenItems(an(item().backstagePass().withSellIn(10).withQuality(10)));
+        givenItem(an(item().backstagePass().withSellIn(10).withQuality(10)));
         whenItemsAreUpdated();
         thenItemQualityIs(12);
     }
 
     @Test
     public void backstagePass_tripleIncreaseInQuality() {
-        givenItems(an(item().backstagePass().withSellIn(5).withQuality(10)));
+        givenItem(an(item().backstagePass().withSellIn(5).withQuality(10)));
         whenItemsAreUpdated();
         thenItemQualityIs(13);
     }
 
     @Test
     public void backstagePass_maxQuality() {
-        givenItems(an(item().backstagePass().withQuality(50)));
+        givenItem(an(item().backstagePass().withQuality(50)));
         whenItemsAreUpdated();
         thenItemQualityIs(50);
     }
 
     @Test
     public void backstagePass_dropToZeroQualityAfterConcert() {
-        givenItems(an(item().backstagePass().withSellIn(0).withQuality(10)));
+        givenItem(an(item().backstagePass().withSellIn(0).withQuality(10)));
         whenItemsAreUpdated();
         thenItemQualityIs(0);
     }
 
     @Test
     public void conjuredItem_qualityDegeneratesTwiceAsFast() {
-        givenItems(an(item().conjuredItem().withQuality(10)));
+        givenItem(an(item().conjuredItem().withQuality(10)));
         whenItemsAreUpdated();
         thenItemQualityIs(8);
     }
 
     @Test
     public void conjuredItem_qualityDegeneratesTwiceAsFastWhenSellInDateIsPassed() {
-        givenItems(an(item().conjuredItem().withSellIn(-1).withQuality(10)));
+        givenItem(an(item().conjuredItem().withSellIn(-1).withQuality(10)));
         whenItemsAreUpdated();
         thenItemQualityIs(6);
     }
 
     @Test
     public void conjuredItem_sellInIsReduced() {
-        givenItems(an(item().conjuredItem().withSellIn(10)));
+        givenItem(an(item().conjuredItem().withSellIn(10)));
         whenItemsAreUpdated();
         thenItemSellInIs(9);
     }
@@ -185,6 +181,18 @@ public class UpdateItemsTest extends UsecaseTest<Request, Response> {
         assertThat(getItem(1).getQuality()).isEqualTo(9);
     }
 
+    private void givenItem(ItemEntity item) {
+        itemGateway.givenItems(item);
+    }
+
+    private void givenItems(ItemEntity ... items) {
+        itemGateway.givenItems(items);
+    }
+
+    private void whenItemsAreUpdated() {
+        whenRequestIsExecuted();
+    }
+
     private void thenItemQualityIs(int expected) {
         assertThat(getItem(0).getQuality()).isEqualTo(expected);
     }
@@ -199,10 +207,6 @@ public class UpdateItemsTest extends UsecaseTest<Request, Response> {
 
     private RequestBuilder request() {
         return new RequestBuilder();
-    }
-
-    private void givenItems(ItemEntity ... items) {
-        itemGateway.givenItems(items);
     }
 
     private static class RequestBuilder implements Builder<Request> {
